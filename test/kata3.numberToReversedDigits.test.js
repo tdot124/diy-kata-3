dec71e71d344ede1250765b2bc21d0ef37537f1c
Tom Mills <t.mills124@gmail.com>
2023-06-13 19:03:03 +0100

    added test for numberToReversedDigits

--- test/kata3.numberToReversedDigits.test.js
+++ test/kata3.numberToReversedDigits.test.js
@@ -1,5 +1,8 @@
 const { numberToReversedDigits } = require("../src");
 
 describe("numberToReversedDigits", () => {
-  test("returns a reversed array of the number's digits", () => {});
+  test("returns a reversed array of the number's digits", () => {
+    expect(numberToReversedDigits(12345)).toBe([5, 4, 3, 2, 1]);
+    expect(numberToReversedDigits(63)).toBe([3, 6]);
+  });
 });
