aef5f418ae2360e8a43908ad20f6e4b5299311b2
Tom Mills <t.mills124@gmail.com>
2023-06-13 20:02:25 +0100

    solved reachDestination kata

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,6 @@
-const reachDestination = (distance, speed) => {};
+const reachDestination = (distance, speed) => {
+  const hours = Math.ceil((distance / speed) * 2) / 2;
+  return `I should be there in ${hours} hours.`;
+};
 
 module.exports = reachDestination;
