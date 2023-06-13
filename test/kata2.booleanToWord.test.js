9db6eab6cd2b871c3250deae03a6f533681e6ff3
Tom Mills <t.mills124@gmail.com>
2023-06-13 18:52:50 +0100

    added tests for booleanToWord

--- test/kata2.booleanToWord.test.js
+++ test/kata2.booleanToWord.test.js
@@ -1,5 +1,10 @@
 const { booleanToWord } = require("../src");
 
 describe("booleanToWord", () => {
-  // how do we create specs again???
+  test("returns Yes when passed true", () => {
+    expect(booleanToWord(true)).toBe("Yes");
+  });
+  test("returns No when passed false", () => {
+    expect(booleanToWord(false)).toBe("No");
+  });
 });
