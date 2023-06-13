dee7cf1ffb8f39b5af7d3ff087594367a35ea524
Tom Mills <t.mills124@gmail.com>
2023-06-13 18:38:52 +0100

    solved FizzBuzz Kata

--- src/kata1.fizzBuzz.js
+++ src/kata1.fizzBuzz.js
@@ -1,3 +1,17 @@
-const fizzBuzz = (number) => {};
+const fizzBuzz = (n) => {
+  let output = "";
+  if (n % 3 === 0) {
+    output += "Fizz";
+  }
+  if (n % 5 === 0) {
+    output += "Buzz";
+  }
+
+  if (output === "") {
+    output = n;
+  }
+
+  return output;
+};
 
 module.exports = fizzBuzz;
