9c08b7fb00ea76791cddf521bf81e946f131760c
Tom Mills <t.mills124@gmail.com>
2023-06-13 20:01:56 +0100

    added test for reachDestination

--- test/kata5.reachDestination.test.js
+++ test/kata5.reachDestination.test.js
@@ -1,5 +1,11 @@
 const { reachDestination } = require("../src");
 
 describe("reachDestination", () => {
-  test("returns string with estimated time of arrival", () => {});
+  test("returns string with estimated time of arrival", () => {
+    expect(reachDestination(44, 10)).toEqual("I should be there in 4.5 hours.");
+    expect(reachDestination(41, 10)).toEqual("I should be there in 4.5 hours.");
+    expect(reachDestination(66, 6)).toEqual("I should be there in 11 hours.");
+    expect(reachDestination(22, 7)).toEqual("I should be there in 3.5 hours.");
+    expect(reachDestination(1, 20)).toEqual("I should be there in 0.5 hours.");
+  });
 });
