54b32fb4665e7ef68c7876bb184c3d8df80f8ba3
Tom Mills <t.mills124@gmail.com>
2023-06-13 19:49:01 +0100

    solved humandogcat years kata

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,23 @@
-const humanCatDogYears = (number) => {};
+const humanCatDogYears = (number) => {
+  let dogYears = 0;
+  let catYears = 0;
+
+  if (number === 1) {
+    dogYears = 15;
+    catYears = 15;
+  }
+
+  if (number === 2) {
+    dogYears = 24;
+    catYears = 24;
+  }
+
+  if (number > 2) {
+    dogYears = (number - 2) * 5 + 24;
+    catYears = (number - 2) * 4 + 24;
+  }
+
+  return [number, catYears, dogYears];
+};
 
 module.exports = humanCatDogYears;
