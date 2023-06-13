75b2df934fd6112b41e9b1d0b0c72a0a46883074
Tom Mills <t.mills124@gmail.com>
2023-06-13 19:34:50 +0100

    updated test to use toStrictEqual

--- test/kata3.numberToReversedDigits.test.js
+++ test/kata3.numberToReversedDigits.test.js
@@ -2,7 +2,7 @@ const { numberToReversedDigits } = require("../src");
 
 describe("numberToReversedDigits", () => {
   test("returns a reversed array of the number's digits", () => {
-    expect(numberToReversedDigits(12345)).toBe([5, 4, 3, 2, 1]);
-    expect(numberToReversedDigits(63)).toBe([3, 6]);
+    expect(numberToReversedDigits(12345)).toStrictEqual([5, 4, 3, 2, 1]);
+    expect(numberToReversedDigits(63)).toStrictEqual([3, 6]);
   });
 });
