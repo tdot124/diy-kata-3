444bddf2146be64861f0a8cf528f52602298d53c
Tom Mills <t.mills124@gmail.com>
2023-06-19 19:15:56 +0100

    added test for getEmployeeRole

--- test/kata7.getEmployerRole.test.js
+++ test/kata7.getEmployerRole.test.js
@@ -1,5 +1,26 @@
 const { getEmployerRole } = require("../src");
 
+const employees = [
+  {
+    name: "Satti",
+    role: "Developer",
+  },
+  {
+    name: "Jenny",
+    role: "Sales Associate",
+  },
+  {
+    name: "Javid",
+    role: "Human Recommended Reading Assistant",
+  },
+];
+
 describe("getEmployerRole", () => {
-  test("returns the employee's role in the company", () => {});
+  test("returns the employee's role in the company", () => {
+    expect(getEmployerRole("Javid", employees)).toEqual(
+      "Human Recommended Reading Assistant"
+    );
+    expect(getEmployerRole("Jenny", employees)).toEqual("Sales Associate");
+    expect(getEmployerRole("Satti", employees)).toEqual("Developer");
+  });
 });
