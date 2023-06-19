92a570cfda8875d0e583c910bfc297bf84f23aec
Tom Mills <t.mills124@gmail.com>
2023-06-19 19:08:49 +0100

    solved joinNames kata

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,16 @@
-const joinNames = (namesObj) => {};
+const joinNames = (namesObj) => {
+  if (namesObj.length > 2) {
+    const start = namesObj.shift().name;
+    const end = ` & ${namesObj.pop().name}`;
+    const middle = namesObj
+      .map((obj) => `, ${obj.name}`)
+      .reduce((acc, person) => acc + person);
+    return `${start + middle + end}`;
+  }
+  if (namesObj.length === 2) {
+    return `${namesObj.shift().name} & ${namesObj.pop().name}`;
+  }
+  return `${namesObj.shift().name}`;
+};
 
 module.exports = joinNames;
