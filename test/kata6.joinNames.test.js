a8c558005bef8083c23273de6a7d20e1f8ade339
Tom Mills <t.mills124@gmail.com>
2023-06-19 18:58:11 +0100

    added test for joinNames Kata

--- test/kata6.joinNames.test.js
+++ test/kata6.joinNames.test.js
@@ -1,5 +1,21 @@
 const { joinNames } = require("../src");
 
 describe("joinNames", () => {
-  test("returns string of names, seperated by commas and an ampersand", () => {});
+  test("returns string of names, seperated by commas and an ampersand", () => {
+    expect(
+      joinNames([{ name: "Bart" }, { name: "Lisa" }, { name: "Maggie" }])
+    ).toEqual("Bart, Lisa & Maggie");
+    expect(
+      joinNames([
+        { name: "Bart" },
+        { name: "Lisa" },
+        { name: "Maggie" },
+        { name: "Dave" },
+      ])
+    ).toEqual("Bart, Lisa, Maggie & Dave");
+    expect(joinNames([{ name: "Bart" }, { name: "Lisa" }])).toEqual(
+      "Bart & Lisa"
+    );
+    expect(joinNames([{ name: "Bart" }])).toEqual("Bart");
+  });
 });
